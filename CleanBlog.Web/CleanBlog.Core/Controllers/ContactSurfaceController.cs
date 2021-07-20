7aee3fd68f6ece4f478bae5a2f82eafdfbf59d7d
Alasdair Selmes <alasdair.selmes@blendinteractive.com>
2021-07-20 14:55:19 -0500

    Contact Form, Dependency Injection, Search

---
+++ CleanBlog.Web/CleanBlog.Core/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using CleanBlog.Core.ViewModels;
+using System.Web;
+using Umbraco.Web;
+using System.Net.Mail;
+using Umbraco.Core.Logging;
+using CleanBlog.Core.Services;
+
+namespace CleanBlog.Core.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        private readonly ISmtpService _smtpService;
+        public ContactSurfaceController(ISmtpService smtpService)
+        {
+            _smtpService = smtpService;
+        }
+
+
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            ContactViewModel model = new ContactViewModel() { ContactFormId = CurrentPage.Id };
+            return PartialView("ContactForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView("ContactForm", model);
+        }
+
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+
+            if (ModelState.IsValid)
+            {
+                success = _smtpService.SendEmail(model);
+            }
+
+            var contactPage = UmbracoContext.Content.GetById(false, model.ContactFormId);
+
+            var successMessage = contactPage.Value<IHtmlString>("successMessage");
+            var errorMessage = contactPage.Value<IHtmlString>("errorMessage");
+
+            return PartialView("Result", success ? successMessage : errorMessage);
+        }
+
+
+    }
+}
